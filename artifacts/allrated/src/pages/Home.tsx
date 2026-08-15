3c706a5c7a5ff24aba12b729701689a1875ed367
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-08-15 18:41:44 +0530

    Implement Bingr home sections from supplied page data

--- artifacts/allrated/src/pages/Home.tsx
+++ artifacts/allrated/src/pages/Home.tsx
@@ -3,8 +3,7 @@ import { useContinueWatching } from "@/hooks/useContinueWatching";
 import { useRegion } from "@/hooks/useRegion";
 import { HeroSection } from "@/components/HeroSection";
 import { ContentTray } from "@/components/ContentTray";
-import { StudiosTray } from "@/components/StudiosTray";
-import { PopularGenresTray } from "@/components/PopularGenresTray";
+import { BingrHomeSections } from "@/components/BingrHomeSections";
 import { PopularLanguagesTray } from "@/components/PopularLanguagesTray";
 import { Seo } from "@/components/Seo";
 
@@ -20,7 +19,6 @@ export default function Home() {
   const anime = useGetAnime();
   const { items: continueTitles } = useContinueWatching();
 
-  // Regional / Indian content
   const regionalMovies = useGetRegional({ mediaType: "movie", country: region });
   const regionalTv = useGetRegional({ mediaType: "tv", country: region });
   const hindiMovies = useGetByLanguage({ mediaType: "movie", language: "hi" });
@@ -36,173 +34,39 @@ export default function Home() {
 
       <div className="relative z-10 pt-6 md:pt-10 space-y-2">
         {continueTitles.length > 0 && (
-          <ContentTray
-            heading="Continue Watching"
-            titles={continueTitles}
-            viewAllHref="/continue-watching"
-            size="md"
-          />
+          <ContentTray heading="Continue Watching" titles={continueTitles} viewAllHref="/continue-watching" size="md" />
         )}
 
-        {/* Regional content first for India */}
         {isIndia && regionalMovies.data && regionalMovies.data.length > 0 && (
-          <ContentTray
-            heading="Indian Movies"
-            titles={regionalMovies.data}
-            loading={regionalMovies.isLoading}
-            viewAllHref="/catalog/regional?mediaType=movie&country=IN"
-            size="md"
-          />
+          <ContentTray heading="Indian Movies" titles={regionalMovies.data} loading={regionalMovies.isLoading} viewAllHref="/catalog/regional?mediaType=movie&country=IN" size="md" />
         )}
-
         {isIndia && regionalTv.data && regionalTv.data.length > 0 && (
-          <ContentTray
-            heading="Indian TV Shows"
-            titles={regionalTv.data}
-            loading={regionalTv.isLoading}
-            viewAllHref="/catalog/regional?mediaType=tv&country=IN"
-            size="md"
-          />
+          <ContentTray heading="Indian TV Shows" titles={regionalTv.data} loading={regionalTv.isLoading} viewAllHref="/catalog/regional?mediaType=tv&country=IN" size="md" />
         )}
-
         {isIndia && hindiMovies.data && hindiMovies.data.length > 0 && (
-          <ContentTray
-            heading="Hindi Movies"
-            titles={hindiMovies.data}
-            loading={hindiMovies.isLoading}
-            viewAllHref="/catalog/language?mediaType=movie&language=hi"
-            size="md"
-          />
+          <ContentTray heading="Hindi Movies" titles={hindiMovies.data} loading={hindiMovies.isLoading} viewAllHref="/catalog/language?mediaType=movie&language=hi" size="md" />
         )}
-
         {isIndia && tamilMovies.data && tamilMovies.data.length > 0 && (
-          <ContentTray
-            heading="Tamil Movies"
-            titles={tamilMovies.data}
-            loading={tamilMovies.isLoading}
-            viewAllHref="/catalog/language?mediaType=movie&language=ta"
-            size="md"
-          />
+          <ContentTray heading="Tamil Movies" titles={tamilMovies.data} loading={tamilMovies.isLoading} viewAllHref="/catalog/language?mediaType=movie&language=ta" size="md" />
         )}
-
         {isIndia && teluguMovies.data && teluguMovies.data.length > 0 && (
-          <ContentTray
-            heading="Telugu Movies"
-            titles={teluguMovies.data}
-            loading={teluguMovies.isLoading}
-            viewAllHref="/catalog/language?mediaType=movie&language=te"
-            size="md"
-          />
+          <ContentTray heading="Telugu Movies" titles={teluguMovies.data} loading={teluguMovies.isLoading} viewAllHref="/catalog/language?mediaType=movie&language=te" size="md" />
         )}
 
-        <ContentTray
-          heading="Trending Right Now"
-          titles={trending.data}
-          loading={trending.isLoading}
-          numbered
-          viewAllHref="/category/Trending%20Now"
-          size="md"
-        />
+        <BingrHomeSections />
 
-        <ContentTray
-          heading="New Movies"
-          titles={popularMovies.data}
-          loading={popularMovies.isLoading}
-          viewAllHref="/category/Popular%20Movies"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Popular TV Shows"
-          titles={popularTv.data}
-          loading={popularTv.isLoading}
-          viewAllHref="/category/Popular%20TV%20Shows"
-          size="md"
-        />
-
-        <StudiosTray />
-
-        <ContentTray
-          heading="Top Rated Movies"
-          titles={topRatedMovies.data}
-          loading={topRatedMovies.isLoading}
-          viewAllHref="/category/Top%20Rated%20Movies"
-          size="md"
-        />
-
-        <PopularGenresTray />
-
-        <ContentTray
-          heading="Blockbuster Action"
-          titles={trendingMovies.data}
-          loading={trendingMovies.isLoading}
-          viewAllHref="/category/Action%20Movies"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Laugh Out Loud"
-          titles={popularMovies.data?.slice().reverse()}
-          loading={popularMovies.isLoading}
-          viewAllHref="/category/Comedy%20Movies"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Sci-Fi & Fantasy"
-          titles={trending.data?.slice(4)}
-          loading={trending.isLoading}
-          viewAllHref="/category/Sci-Fi%20Movies"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Anime Series"
-          titles={anime.data}
-          loading={anime.isLoading}
-          viewAllHref="/anime"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Spine-Chilling Horror"
-          titles={trendingMovies.data?.slice(2)}
-          loading={trendingMovies.isLoading}
-          viewAllHref="/category/Horror%20Movies"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Heartwarming Romance"
-          titles={popularMovies.data?.slice(3)}
-          loading={popularMovies.isLoading}
-          viewAllHref="/category/Romance%20Movies"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Top Rated TV"
-          titles={topRatedTv.data}
-          loading={topRatedTv.isLoading}
-          viewAllHref="/category/Top%20Rated%20TV%20Shows"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Trending TV"
-          titles={trendingTv.data}
-          loading={trendingTv.isLoading}
-          viewAllHref="/category/Trending%20TV%20Shows"
-          size="md"
-        />
-
-        <ContentTray
-          heading="Crime Thrillers"
-          titles={trending.data?.slice(1)}
-          loading={trending.isLoading}
-          viewAllHref="/category/Crime%20Movies"
-          size="md"
-        />
+        <ContentTray heading="New Movies" titles={popularMovies.data} loading={popularMovies.isLoading} viewAllHref="/category/Popular%20Movies" size="md" />
+        <ContentTray heading="Popular TV Shows" titles={popularTv.data} loading={popularTv.isLoading} viewAllHref="/category/Popular%20TV%20Shows" size="md" />
+        <ContentTray heading="Top Rated Movies" titles={topRatedMovies.data} loading={topRatedMovies.isLoading} viewAllHref="/category/Top%20Rated%20Movies" size="md" />
+        <ContentTray heading="Blockbuster Action" titles={trendingMovies.data} loading={trendingMovies.isLoading} viewAllHref="/category/Action%20Movies" size="md" />
+        <ContentTray heading="Laugh Out Loud" titles={popularMovies.data?.slice().reverse()} loading={popularMovies.isLoading} viewAllHref="/category/Comedy%20Movies" size="md" />
+        <ContentTray heading="Sci-Fi & Fantasy" titles={trending.data?.slice(4)} loading={trending.isLoading} viewAllHref="/category/Sci-Fi%20Movies" size="md" />
+        <ContentTray heading="Anime Series" titles={anime.data} loading={anime.isLoading} viewAllHref="/anime" size="md" />
+        <ContentTray heading="Spine-Chilling Horror" titles={trendingMovies.data?.slice(2)} loading={trendingMovies.isLoading} viewAllHref="/category/Horror%20Movies" size="md" />
+        <ContentTray heading="Heartwarming Romance" titles={popularMovies.data?.slice(3)} loading={popularMovies.isLoading} viewAllHref="/category/Romance%20Movies" size="md" />
+        <ContentTray heading="Top Rated TV" titles={topRatedTv.data} loading={topRatedTv.isLoading} viewAllHref="/category/Top%20Rated%20TV%20Shows" size="md" />
+        <ContentTray heading="Trending TV" titles={trendingTv.data} loading={trendingTv.isLoading} viewAllHref="/category/Trending%20TV%20Shows" size="md" />
+        <ContentTray heading="Crime Thrillers" titles={trending.data?.slice(1)} loading={trending.isLoading} viewAllHref="/category/Crime%20Movies" size="md" />
 
         <PopularLanguagesTray />
       </div>
