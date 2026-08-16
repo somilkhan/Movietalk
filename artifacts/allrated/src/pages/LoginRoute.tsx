3759985d3c8e2d25100e4611727e9c45fa4f6c37
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-08-17 01:14:59 +0530

    Wire last-login indicator into login route

---
+++ artifacts/allrated/src/pages/LoginRoute.tsx
@@ -0,0 +1,6 @@
+import Login from "@/pages/Login";
+import { LastLoginHint } from "@/components/LastLoginHint";
+
+export default function LoginRoute() {
+  return <><Login /><LastLoginHint /></>;
+}
